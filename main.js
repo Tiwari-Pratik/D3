59b5b794700a2e3d25195308de0a8a3a499bde6f
pratik <pratiktiwari3792@gmail.com>
2023-01-13 17:55:29 +0530

    Worked on various modifying methods of D3

--- main.js
+++ main.js
@@ -283,3 +283,164 @@ styleEllipses.style("fill", function(d, i, n) {
 });
 
 // property methods
+
+const labelsFor = ["fname", "lname", "gender", "male", "female", "nomention"];
+const labels = d3.selectAll("label");
+labels.filter(function(d, i, n) {
+  const d3Label = d3.select(this);
+  d3Label.attr("for", labelsFor[i]);
+});
+
+d3.select("#fname").property("type", "text").property("placeholder", "John");
+d3.select("#lname").property("type", "text").property("placeholder", "Doe");
+d3.select("#submit").property("type", "submit").property("value", "Submit");
+
+const genderInputs = d3.selectAll("#formsubmit > div input");
+genderInputs.property("type", "checkbox");
+const genderValues = ["male", "female", "nomention"];
+genderInputs.property("value", function(d, i, n) {
+  return genderValues[i];
+});
+
+const nomention = d3.select("#nomention");
+nomention.property("checked", "true");
+
+document.querySelector("#formsubmit").addEventListener("submit", () => {
+  event.preventDefault();
+  console.log(d3.select("#fname").property("value"));
+  console.log(d3.select("#lname").property("value"));
+  console.log(d3.select("#male").property("checked"));
+  console.log(d3.select("#female").property("checked"));
+  console.log(d3.select("#nomention").property("checked"));
+});
+
+// text method
+
+const texts = d3.selectAll(".svgtext text");
+texts.text(function(d, i, n) {
+  return `Text ${i + 1}`;
+});
+
+// append method
+
+const titleDiv = d3.select("#mytitle");
+console.log({ titleDiv });
+titleDiv.style("min-height", "300px").style("background-color", "#787679");
+titleDiv.append("h1").text("Append");
+
+const rectsData = [
+  {
+    x: "10",
+    y: "100",
+    width: "50",
+    height: "30",
+  },
+
+  {
+    x: "10",
+    y: "150",
+    width: "50",
+    height: "30",
+  },
+];
+
+const svgRects = d3.select("#svgtitle");
+
+const rectSvg = svgRects
+  .append("svg")
+  .attr("width", "300")
+  .attr("height", "300");
+
+for (let i = 0; i < rectsData.length; i++) {
+  const rect = rectSvg.append("rect");
+  rect
+    .attr("x", rectsData[i].x)
+    .attr("y", rectsData[i].y)
+    .attr("width", rectsData[i].width)
+    .attr("height", rectsData[i].height);
+}
+
+// html method
+
+const htmlDiv = d3.select("#htmldiv");
+htmlDiv.html("<h2>This line is from D3</h2>");
+
+htmlDiv.html(function(d, i, n) {
+  let content = "";
+  for (let i = 0; i < 5; i++) {
+    content += `<p>Para with index: ${i} </p>`;
+  }
+  return content;
+});
+
+// insert method
+
+const newsvg1 = d3.select("#newsvg1");
+newsvg1
+  .insert("text")
+  .text("This line was inserted with text method")
+  .attr("x", "5")
+  .attr("y", "40");
+newsvg1
+  .insert("text", "text")
+  .text("This line was inserted with text method using before selector")
+  .attr("x", "5")
+  .attr("y", "20");
+
+const newsvg2 = d3.select("#newsvg2");
+newsvg2
+  .insert(
+    function() {
+      return document.createElementNS("http://www.w3.org/2000/svg", "text");
+    },
+    function() {
+      return this.firstElementChild;
+    }
+  )
+  .text("This is a text element.")
+  .attr("x", "10")
+  .attr("y", "50");
+
+// remove method
+
+const svgCircle = d3.select(".svgremove circle");
+const removedCircle = svgCircle.remove();
+console.log({ removedCircle });
+
+// clone method
+
+const thesvg = d3.select(".svgclone");
+console.log({ thesvg });
+const clonedSvg = thesvg.clone();
+
+console.log({ clonedSvg });
+
+const deepClonedsvg = thesvg.clone(true);
+console.log({ deepClonedsvg });
+
+// sort method
+
+const paras = d3.selectAll("#parasort p");
+paras.datum(function() {
+  return this.innerText;
+});
+
+console.log({ paras });
+
+paras.sort((a, b) => b - a);
+
+d3.selectAll(".sortsvg circle")
+  .datum(function() {
+    return d3.select(this).attr("r");
+  })
+  .sort((a, b) => a - b)
+  .attr("cx", function(d, i, n) {
+    return 40 + i * 100;
+  });
+
+// raise method and lower method
+
+d3.select("#svgraise rect:nth-of-type(1)").raise();
+d3.select("#svgraise rect:nth-of-type(2)").raise();
+
+d3.select("#svgraise rect:nth-of-type(3)").lower();
